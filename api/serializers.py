c78c78461bf957d66f5b0c0580a86e1513835275
bachts <tunfbachvn2003@gmail.com>
2023-07-04 09:47:48 +0700

    viewset api

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,32 @@
 from rest_framework import serializers
-from OKR.models import OKR, Log
+from OKR import models
 
-# class OKRSerializers(serializers.ModelSerializer):
-#     id = serializers
-#     class Meta:
-#         model = OKR
-#         fields = ('id', 'objective_id', 'created_by', 'status', 'deadline')
+class OKRSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = models.OKR
+        fields = ('id', 'objective_id', 'created_by', 'status', 'deadline')
 
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Log
+        model = models.Log
         fields = ('okr_id', 'updated_by', 'updated_at')
+
+class KeyResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.KeyResult
+        fields = ('id', 'okr_id', 'key_result_name')
+
+class ObjectiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Objective
+        fields = ('id', 'objective_name')
+
+class FormulaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Formula
+        fields = ('id', 'formula_name', 'formula_value')
+
+class SourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Source
+        fields = ('id', 'source_name')
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,21 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register(r'log', views.LogViewSet, basename='log')
+router.register(r'source', views.SourceViewSet, basename='source')
+router.register(r'formula', views.FormulaViewSet, basename='formula')
+router.register(r'objective', views.ObjectiveViewSet, basename='objective')
+router.register(r'keyresult', views.KeyResultViewSet, basename='keyresult')
+router.register(r'okr', views.OkrViewSet, basename='okr')
+
+
+
 
 urlpatterns = [
     #path('okr/get', views.get_okr, name='get-okr'),
-    path('log/', views.log_list, name='log-list'),
-    path('log/<int:pk>/', views.log_detail, name='log-detail')
+    path('', include(router.urls)),
+    # path('', views.home.as_view),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
