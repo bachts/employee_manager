a65b58ed3eba5a080d5ec02dad27d81362de3a81
bachts <tunfbachvn2003@gmail.com>
2023-07-04 08:55:36 +0700

    Api view for some models

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,32 @@
 from rest_framework import serializers
-from OKR.models import OKR, Log
+from OKR.models import OKR, Log, Objective, Formula, Source, KeyResult
 
-# class OKRSerializers(serializers.ModelSerializer):
-#     id = serializers
-#     class Meta:
-#         model = OKR
-#         fields = ('id', 'objective_id', 'created_by', 'status', 'deadline')
+class OKRSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = OKR
+        fields = ('id', 'objective_id', 'created_by', 'status', 'deadline')
 
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
-        fields = ('okr_id', 'updated_by', 'updated_at')
+        fields = ('id', 'okr_id', 'updated_by', 'updated_at')
+
+class ObjectiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Objective
+        fields = ('id', 'objective_name', 'objective_content')
+
+class FormulaSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Formula
+        fields = ('id', 'formula_name', 'formula_value')
+
+class SourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Formula
+        fields = ('id', 'source_name')
+
+class KeyResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KeyResult
+        fields = ('id', 'okr_id', 'key_result_name')
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
-from OKR.models import OKR, Log
+from OKR.models import OKR, Log, Objective, Source, KeyResult, Formula
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -8,7 +8,7 @@ from rest_framework import status, permissions, mixins
 from rest_framework.views import APIView
 
 
-from .serializers import  LogSerializer#, OKRSerializers
+from .serializers import  LogSerializer, ObjectiveSerializer, KeyResultSerializer, OKRSerializers, FormulaSerializer, SourceSerializer
 # Create your views here.
 
 
@@ -25,33 +25,34 @@ from .serializers import  LogSerializer#, OKRSerializers
 #         return Response(serializer.data)
 #     else: 
 #         return Response(status=status.HTTP_404_NOT_FOUND)
-class LogList(APIView):
-    
-    def get(self, request, format=None):
-        logs = Log.objects.all()
-        serializer = LogSerializer(logs, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request, format=None):
-        serializer = LogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class LogDetail(APIView):
+class LogAPI(APIView):
     
     def get_object(self, pk):
         try:
             return Log.objects.get(pk=pk)
         except Log.DoesNotExist:
             raise Http404
+    #RETRIEVE ALL
+    def get_all(self, request, format=None):
+        logs = Log.objects.all()
+        serializer = LogSerializer(logs, many=True)
+        return Response(serializer.data)
+    
+    #CREATE
+    def post(self, request, format=None):
+        serializer = LogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+    #RETRIEVE BY PK
     def get(self, response, pk, format=None):
         log = self.get_object(pk)
         serializer = LogSerializer(log)
         return Response(serializer.data)
     
+    #UPDATE
     def put(self, request, pk, format=None):
         log = self.get_object(pk)
         serializer = LogSerializer(log, data=request.data)
@@ -60,7 +61,84 @@ class LogDetail(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    #DELETE
     def delete(self, request, pk, format=None):
         log = self.get_object(pk)
         log.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ObjectiveAPI(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return Objective.objects.get(pk=pk)
+        except Objective.DoesNotExist:
+            raise Http404
+    # RETRIEVE ALL
+    def get_all(self, request, format=None):
+        objectives = Objective.objects.all()
+        serializer =  ObjectiveSerializer(objectives, many=True)
+        return Response(objectives.data)
+    # CREATE
+    def post(self, request, format=None):
+        serializer = ObjectiveSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #RETRIEVE
+    def get(self, response, pk, format=None):
+        objective = self.get_object(pk)
+        serializer = ObjectiveSerializer(objective)
+        return Response(serializer.data)
+    #UPDATE
+    def put(self, request, pk, format=None):
+        objective = self.get_object(pk)
+        serializer = ObjectiveSerializer(objective, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #DELETE
+    def delete(self, request, pk, format=None):
+        objective = self.get_object(pk)
+        objective.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class SourceAPI(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return Source.objects.get(pk=pk)
+        except Source.DoesNotExist:
+            raise Http404
+    # RETRIEVE ALL
+    def get_all(self, request, format=None):
+        sources = Objective.objects.all()
+        serializer =  SourceSerializer(sources, many=True)
+        return Response(sources.data)
+    # CREATE
+    def post(self, request, format=None):
+        serializer = ObjectiveSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #RETRIEVE
+    def get(self, response, pk, format=None):
+        source = self.get_object(pk)
+        serializer = SourceSerializer(source)
+        return Response(serializer.data)
+    #UPDATE
+    def put(self, request, pk, format=None):
+        source = self.get_object(pk)
+        serializer = ObjectiveSerializer(source, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #DELETE
+    def delete(self, request, pk, format=None):
+        source = self.get_object(pk)
+        source.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
